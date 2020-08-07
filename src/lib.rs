d818f52f8159bfb4f1c88f5c54088ebf55eed33e
EmisonLu <lzh1633856298@gmail.com>
2020-08-07 10:42:46 +0800

    no message

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+// use std::prelude::v1::*;
+pub use crate::{
+    options::TokenizerOption, stream::CangjieTokenStream, tokenizer::CangJieTokenizer,
+};
+
+pub mod options;
+pub mod stream;
+pub mod tokenizer;
+
+pub const CANG_JIE: &str = "CANG_JIE";
